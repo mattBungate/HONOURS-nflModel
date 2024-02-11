4349d0864b2867f364cd540d4564a388185b56d6
mattBungate MacBook <matt.bungate@gmail.com>
2024-02-11 12:16:09 +1000

    add: order_action.jl

---
+++ order_actions.jl
@@ -0,0 +1,64 @@
+function get_feasible_actions(
+    state::State
+)::Vector{String}
+    feasible_actions = []
+    if state.ball_section > FIELD_GOAL_CUTOFF
+        push!(feasible_actions, "Field Goal")
+    end
+    if state.down != 4
+        if state.clock_ticking
+            push!(feasible_actions, "Kneel")
+            push!(feasible_actions, "Spike")
+        end
+    elseif state.ball_section < flip_field(TOUCHBACK_SECTION)
+        push!(feasible_actions, "Punt")
+    end
+    if state.timeouts_remaining[1] > 0 && state.clock_ticking
+        push!(feasible_actions, "Timeout")
+        if state.seconds_remaining > 1
+            push!(feasible_actions, "Delayed Timeout")
+        end
+    end
+    push!(feasible_actions, "Hurried Play")
+    if state.seconds_remaining > 1
+        push!(feasible_actions, "Delayed Play")
+    end
+    
+    return feasible_actions
+end
+
+function order_actions(
+    state::State,
+    best_move::String
+)::Vector{String}
+    feasible_actions = get_feasible_actions(state)
+
+    action_space_ordered = []
+    if state.score_diff > 0
+        for action in feasible_actions
+            if action in feasible_actions
+                push!(action_space_ordered, action)
+            end
+        end
+    else
+        for action in reverse(action_space)
+            if action in feasible_actions
+                push!(action_space_ordered, action)
+            end
+        end
+    end
+
+    # Move best move to front 
+    if best_move == ""
+        return action_space_ordered
+    else
+        ordered_moves = [best_move]
+        for action in action_space
+            if action == best_move
+                continue
+            end
+            push!(ordered_moves, action)
+        end
+        return ordered_moves
+    end
+end
